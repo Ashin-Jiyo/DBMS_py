950e4f5f90ccad44063067c9a7562a950aa0615e
Ashin-Jiyo <20527@bpsdoha.edu.qa>
2026-01-20 15:45:00 +0300

    first commit

--- test2.py
+++ test2.py
@@ -1,9 +1,13 @@
-def box2(a,b):
+def box(a,b):
     lena=len(a)+2
     lenb = len(b) + 2
     print("","-"*lena,"-"*lenb,"",sep=" ")
     print(f"| {a} | {b} |")
     print("","-"*lena,"-"*lenb,"",sep=" ")
+box("Hello","World")
+
+
+def box22(a,b):
 
     print(" ",end="")
     print("-" * lena,end="")
@@ -24,4 +28,4 @@ def box2(a,b):
     print(" ")
 
 
-box2("Hello","World")
+box("Hello","World")
